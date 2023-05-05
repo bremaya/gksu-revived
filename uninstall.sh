46f6720bbb101b740e58b4403cfe982cb92cb322
Breno Maia <39398165+bremaya@users.noreply.github.com>
2023-05-05 18:25:40 -0300

    Update uninstall.sh
    Minor changes

--- uninstall.sh
+++ uninstall.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo dpkg -P gksu libgksu2-0 libgnome-keyring0 libgnome-keyring-common libgtopgksu-2.0-10-11 multiarch-support
+sudo dpkg -P gksu libgksu2-0 libgnome-keyring0 libgnome-keyring-common libgtopgksu-2.0-10-11 multiarch-support libgdk-pixbuf2.0-0 gconf2 gconf-service
